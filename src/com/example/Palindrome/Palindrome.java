69bac0aacb0e70ff934e2702078a80e3348c6c5a
Planxty-Dan <dan.newport@detroitlabs.com>
2014-09-15 00:24:17 -0400

    add src dir, add project, add Java class, import scanner, add scanner myscan

---
+++ src/com/example/Palindrome/Palindrome.java
@@ -0,0 +1,10 @@
+package com.example.Palindrome;
+
+/**
+ * Created by admin on 9/15/14.
+ */
+import java.util.Scanner;
+
+public class Palindrome {
+    Scanner myScan = new Scanner(System.in);
+}
