977905cd6895aa1bf354e06e809ecfecf2ac6649
Planxty-Dan <dan.newport@detroitlabs.com>
2014-09-15 01:17:43 -0400

    add two strings for input and reverse input, add boolean for while loop of program, add print statement to prompt user, add toLowerCase method, add replace method for spaces in input, add reverse method, add if/else for mathing userInput and reverseInput, add print to prompt user to play again, add scan for user to play again, add while break for user not playing again, add print statments to check methods throughout code

--- src/com/example/Palindrome/Palindrome.java
+++ src/com/example/Palindrome/Palindrome.java
@@ -6,5 +6,38 @@ package com.example.Palindrome;
 import java.util.Scanner;
 
 public class Palindrome {
-    Scanner myScan = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        Scanner myScan = new Scanner(System.in);
+        String userInput;
+        String reverseInput;
+        Boolean progLoop = true;
+        String tryAgain;
+
+        System.out.println("I will tell you if a word or phrase is a palindrome.\n" +
+                "I will do this because I am nice.\n");
+
+        while (progLoop) {
+            System.out.println("Type a word or phrase:\n");
+
+            userInput = myScan.nextLine();
+
+            userInput = userInput.toLowerCase();
+            userInput = userInput.replaceAll(" ", "");
+            System.out.println(userInput);
+            reverseInput = new StringBuffer(userInput).reverse().toString();
+            System.out.println(reverseInput);
+
+            if (userInput.equals(reverseInput))
+                System.out.println("That is a palindrome, yay!");
+            else
+                System.out.println("That isn't a palindrome, sorry.");
+
+            System.out.println("Want to try another word? (y/n)");
+
+            tryAgain = myScan.nextLine();
+            if (tryAgain.toLowerCase().equals("n"))
+                progLoop = false;
+        }
+    }
 }
